9dee59f6f2059c7d7762539a378f049162ebc71d
choozii <jiyechoo@gmail.com>
2022-04-16 13:16:11 +0900

    [TIL] everyday chanllenge problem

--- leetcode/189_Rotate_Array.js
+++ leetcode/189_Rotate_Array.js
@@ -4,3 +4,20 @@ var rotate = function (nums, k) {
   let new_k = k % nums.length;
   nums.unshift(...nums.splice(nums.length - new_k));
 };
+
+var rotate = function (nums, k) {
+  k %= nums.length;
+
+  let reverse = function (i, j) {
+    while (i < j) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      i++;
+      j--;
+    }
+  };
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
+};
